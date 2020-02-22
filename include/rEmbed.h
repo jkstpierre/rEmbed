a58a77a5105cbd71a658b76b71348e2b9e174ed4
Joseph St. Pierre <jkstpierre@wpi.edu>
2020-02-22 11:37:46 -0500

    Removed junk

---
+++ include/rEmbed.h
@@ -0,0 +1,17 @@
+/**
+ * @file rEmbed.h
+ * @author jkstpierre (jkstpierre@wpi.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2020-02-22
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef __REMBED_H__
+#define __REMBED_H__
+
+
+
+#endif
---
+++ src/rEmbed.c
@@ -0,0 +1,25 @@
+/**
+ * @file rEmbed.c
+ * @author jkstpierre (jkstpierre@wpi.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2020-02-22
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief Main function serves as program entry point
+ * 
+ * @param argc    Number of arguments received
+ * @param args    Array of C-strings containing each argument
+ * @return int    Return 0 on success
+ */
+int main(int argc, char** args) {
+  
+}
